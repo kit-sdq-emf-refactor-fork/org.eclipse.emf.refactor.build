57cd8801518716b057bb24315ad3c040a220a498
tarendt <tarendt>
2013-01-04 13:18:28 +0100

    examples builded

---
+++ org.eclipse.emf.refactor.build.examples/build/web-eclipse/downloads_1.php
@@ -0,0 +1,19 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+
+	$pageTitle 		= "EMF Refactor - Downloads";
+	$pageKeywords	= "EMF, Refactor, model refactoring, refactoring";
+	$pageAuthor		= "Thorsten Arendt";
+	
+	$html = <<<EOHTML
+
+<div id="maincontent">
+
+	<div id="midcolumn">
+		<h1>$pageTitle</h1>
+		<p>
+	<h2>Releases</h2>
+		
+<h3>Version 0.7.0</h3>
+<ul class="midlist">
+    <li><a href="downloads/refactor-examples-0.7.0.20130104130920.jar">refactor-examples-0.7.0.20130104130920.jar</a> (released Jan 04, 2013 13:13)</li>
+</ul>
