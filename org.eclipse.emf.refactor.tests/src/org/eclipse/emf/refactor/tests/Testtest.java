1575652ea88e03e15d005aaab4158261659d45f3
René <mail@renehahn.de>
2017-12-22 15:35:37 +0100

    adds new buildscripts and removes the old ones

---
+++ org.eclipse.emf.refactor.tests/src/org/eclipse/emf/refactor/tests/Testtest.java
@@ -0,0 +1,14 @@
+package org.eclipse.emf.refactor.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Testtest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
